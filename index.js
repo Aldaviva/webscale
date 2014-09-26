6b7b346da300b1fb69434a87e44c9a3f4e42eca5
Ben Hutchison <ben@bluejeans.com>
2014-09-26 08:09:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+var HID = require('node-hid');
+var util = require('util');
+var events = require('events');
+
+var VENDOR_ID = 9332;
+var PRODUCT_ID = 1360;
+
+
+function WebScale(deviceIndex){
+    this._prevWeight = null;
+    this._deviceIndex = deviceIndex || 0;
+
+    this._connectWithRetry();
+}
+
+util.inherits(WebScale, events.EventEmitter);
+
+WebScale.prototype._connectWithRetry = function(){
+    var reconnectInterval = setInterval((function(){
+	try {
+	    this._connect();
+	    clearInterval(reconnectInterval);
+	} catch(e){
+	    //reconnect failed, try again shortly
+	}
+    }).bind(this), 2000);
+};
+
+WebScale.prototype._connect = function(){
+    var devices = HID.devices(VENDOR_ID, PRODUCT_ID);
+
+    if(!devices.length || !devices[this._deviceIndex]){
+	this.emit('disconnected');
+    }
+
+    var devicePath = devices[this._deviceIndex].path;
+    var device = new HID.HID(devicePath);
+    this._onConnect(device);
+};
+
+WebScale.prototype._onConnect = function(device){
+    this.emit('connected');
+    device.on('data', this._onData.bind(this));
+    device.on('error', this._onError.bind(this));
+};
+
+WebScale.prototype._onData = function(data){
+    var ounces = data.readInt16LE(4)/10;
+
+    if(this._prevWeight !== ounces){
+	this._prevWeight = ounces;
+	this.emit('change:weight', ounces);
+    }
+};
+
+WebScale.prototype._onError = function(error){
+    if(error.message == 'could not read from HID device'){
+	this.emit('disconnected', error);
+	this._connectWithRetry();
+    } else {
+	this.emit('error', error);
+    }
+};
+
+module.exports = WebScale;
---
+++ test-client.js
@@ -0,0 +1,30 @@
+var WebScale = require('./index');
+
+var webScale = new WebScale();
+
+webScale.on('change:weight', function(ounces){
+    var pounds = roundTowardsZero(ounces/16);
+    var remainderOunces = (Math.round(ounces % 16 * 10)/10).toFixed(1);
+
+    console.log(pounds + " lbs. " + remainderOunces + " oz.");
+});
+
+webScale.on('error', function(err){
+    console.error("Oh noes.", err);
+});
+
+webScale.on('connected', function(){
+    console.log("Scale online.");
+});
+
+webScale.on('disconnected', function(){
+    console.warn("Scale disconnected. Reconnecting...");
+});
+
+function roundTowardsZero(num){
+    if(num >= 0){
+	return Math.floor(num);
+    } else {
+	return Math.ceil(num);
+    }
+}
