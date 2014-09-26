b18f019d27ca39939a424693f12c80e2ea5acea9
Ben Hutchison <ben@bluejeans.com>
2014-09-26 08:43:43 +0000

    Added disconnect()

--- index.js
+++ index.js
@@ -9,11 +9,20 @@ var PRODUCT_ID = 1360;
 function WebScale(deviceIndex){
     this._prevWeight = null;
     this._deviceIndex = deviceIndex || 0;
+    this._device = null;
 
     this._connectWithRetry();
 }
 
 util.inherits(WebScale, events.EventEmitter);
+
+WebScale.prototype.disconnect = function(){
+    if(this._device){
+	this.emit('disconnected');
+	this._device.close();
+	this._device = null;
+    }
+});
 
 WebScale.prototype._connectWithRetry = function(){
     var reconnectInterval = setInterval((function(){
@@ -34,8 +43,8 @@ WebScale.prototype._connect = function(){
     }
 
     var devicePath = devices[this._deviceIndex].path;
-    var device = new HID.HID(devicePath);
-    this._onConnect(device);
+    this._device = new HID.HID(devicePath);
+    this._onConnect(this._device);
 };
 
 WebScale.prototype._onConnect = function(device){
